8a271f9127958588e1eb546859260a99121154ca
xvlirong <717814532@qq.com>
2019-08-27 16:17:23 +0800

    配置信息修改

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -1,4 +1,14 @@
 <?php
 return array(
 	//'配置项'=>'配置值'
+    define('HTTP_TYPE',((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') || (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https')) ? 'https://' : 'http://'),
+    //'配置项' =>'配置值'
+    'MODULE_ALLOW_LIST' =>    array('Home','Admin','Wechat','App'),
+    //我们用了入口版定 所以下面这行可以注释掉
+    //'SHOW_PAGE_TRACE'   =>  true,
+    'LOAD_EXT_CONFIG'   => 'db', //加载数据库配置
+    'URL_CASE_INSENSITIVE'  =>  true,  //url不区分大小写
+    'URL_ROUTER_ON' => true, //URL路由
+    'URL_MODEL'   =>2,
+    'URL_HTML_SUFFIX'  =>'html'
 );
