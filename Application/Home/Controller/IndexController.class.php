3974066f1879cb03a5c771ea7c750c6452eca5f3
xvlirong <717814532@qq.com>
2019-08-27 16:25:23 +0800

    数据库测试

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -3,6 +3,6 @@ namespace Home\Controller;
 use Think\Controller;
 class IndexController extends Controller {
     public function index(){
-        echo phpinfo();
+        echo M("rv_users")->getField('username');
     }
 }
