fa84b9409c47fc7df6419ff3500aefbbb0758a00
xvlirong <717814532@qq.com>
2019-09-06 15:33:10 +0800

    qqq

---
+++ Application/Backend/Controller/VerifyCodeController.class.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Backend\Controller;
+
+class VerifyCodeController extends CommonController
+{
+    public function verify_list()
+    {
+        $list = M("msg_code_log")->order(array('add_time','desc'))->limit(100)->select();
+        $this->assign('list',$list);
+
+        $this->display();
+    }
+
+    public function add_code()
+    {
+        $this->display();
+    }
+
+}
