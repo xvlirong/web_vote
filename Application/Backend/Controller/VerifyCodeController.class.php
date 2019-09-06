923b68b4e41316c3dc888f28c819b25f73c4f229
xvlirong <717814532@qq.com>
2019-09-06 15:34:41 +0800

    111

--- Application/Backend/Controller/VerifyCodeController.class.php
+++ Application/Backend/Controller/VerifyCodeController.class.php
@@ -6,7 +6,7 @@ class VerifyCodeController extends CommonController
 {
     public function verify_list()
     {
-        $list = M("msg_code_log")->order(array('add_time','desc'))->limit(100)->select();
+        $list = M("msg_code_log")->order(array('add_time'=>'desc'))->limit(100)->select();
         $this->assign('list',$list);
 
         $this->display();
