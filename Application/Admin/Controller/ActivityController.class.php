b871144236e8b9fdb7ecb175ca4d5fbd69ce7d51
xvlirong <717814532@qq.com>
2020-04-22 14:05:31 +0800

    111

--- Application/Admin/Controller/ActivityController.class.php
+++ Application/Admin/Controller/ActivityController.class.php
@@ -25,7 +25,7 @@ class ActivityController extends CommonController
      */
     public function act_list()
     {
-        $act_list = M("activity")->order(array('start_time'=>'desc','id'=>'desc'))->select();
+        $act_list = M("activity")->order(array('id'=>'desc'))->select();
         $this->assign('list',$act_list);
 
         $this->display();
