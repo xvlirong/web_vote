1e5d6812abe23106833cb47fc3d5e7fe81448647
xvlirong <717814532@qq.com>
2020-08-28 14:30:23 +0800

    11112

--- Application/SaleMS/Controller/DmController.class.php
+++ Application/SaleMS/Controller/DmController.class.php
@@ -148,7 +148,7 @@ class DmController extends CommonController {
 
         $maps['belong_state'] = array('EQ',0);
         $count = M("sms_user_data")->where($maps)->count();
-        $Page = new \Extend\Page($count,10);// 实例化分页类 传入总f记录数和每页显示的记录数(25)
+        $Page = new \Extend\Page($count,50);// 实例化分页类 传入总f记录数和每页显示的记录数(25)
         $show = $Page->show($map_str);// 分页显示输出
         $list = M("sms_user_data")
             ->where($maps)
@@ -246,7 +246,7 @@ class DmController extends CommonController {
         }
         $maps['belong_state'] = array("EQ",1);
         $count = M("sms_offer_record")->where($maps)->count();
-        $Page = new \Extend\Page($count,10);// 实例化分页类 传入总f记录数和每页显示的记录数(25)
+        $Page = new \Extend\Page($count,50);// 实例化分页类 传入总f记录数和每页显示的记录数(25)
         $show = $Page->show($map_str);// 分页显示输出
         $list = M("sms_offer_record")
             ->join("left join sms_user_data on sms_offer_record.pid=sms_user_data.id")
