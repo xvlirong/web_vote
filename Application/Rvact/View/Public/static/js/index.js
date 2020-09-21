aac97f142e6d96355461ca87efcb455755b1b0f1
xvlirong <717814532@qq.com>
2020-09-21 15:05:57 +0800

    1112

--- Application/Rvact/View/Public/static/js/index.js
+++ Application/Rvact/View/Public/static/js/index.js
@@ -83,7 +83,7 @@ var brand={
     },
     move:function(iNow){
         if(iNow==3){
-            $("#wrap").height(1800)
+            $("#wrap").height(3100)
         }else{
             $("#wrap").height(3840)
         }
